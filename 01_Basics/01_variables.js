16f68d4a67c118ff7f96e4f6d289ac5f04a55a78
Bhargav2503 <101362697+Bhargav2503@users.noreply.github.com>
2024-01-07 17:34:13 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "bhargav@google.com"
+var accountPassword = "123456"
+accountCity = "Macedonia"
+
+//accountId = 2 // Not allowed
+
+accountEmail = "hchc@gmail.com"
+accountPassword = "151654"
+accountCity = " Cleveland"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,11 @@
+ //Number
+ //bigint - for bigger numbers
+ //string = ""
+ //boolean = true/false
+ //null = standalone value
+ //undefined = value not assigned
+ //symbol= unique
+
+ //object
+
+ console.log(typeof);
